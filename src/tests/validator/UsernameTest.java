c6ea133010dbcd704c4f797dccf694ea8869a327
Himanshu Bhaisare <himanshu.bhaisare@gmail.com>
2017-05-01 10:39:46 -0700

    Tests for username validator

--- src/tests/validator/UsernameTest.java
+++ src/tests/validator/UsernameTest.java
@@ -1,6 +1,7 @@
 package tests.validator;
 
 import org.junit.Test;
+import validator.Username;
 
 import static org.junit.Assert.assertEquals;
 
@@ -10,8 +11,37 @@ public class UsernameTest {
     }
 
     @Test
-    public void testYouAreSmart() {
-        assertEquals("You are smart", true, true);
+    public void testUsernameSmallerThan2Chars() {
+        String username = "A";
+        boolean result = Username.validate(username);
+        assertEquals("testUsernameSmallerThan2Chars", false, result);
     }
 
+    @Test
+    public void testUsernameGreaterThan15Chars() {
+        String username = "HimanshuVasantBhaisareTheSecond";
+        boolean result = Username.validate(username);
+        assertEquals("testUsernameGreaterThan15Chars", false, result);
+    }
+    
+    @Test
+    public void testUsernameWithAllCaps() {
+        String username = "HIMANSHU";
+        boolean result = Username.validate(username);
+        assertEquals("testUsernameWithAllCaps", true, result);
+    }
+
+    @Test
+    public void testUsernameWithAllSmalls() {
+        String username = "himanshu";
+        boolean result = Username.validate(username);
+        assertEquals("testUsernameWithAllSmalls", true, result);
+    }
+
+    @Test
+    public void testUsernameWithLettersAndNumbers() {
+        String username = "Himanshu123";
+        boolean result = Username.validate(username);
+        assertEquals("testUsernameWithLettersAndNumbers", true, result);
+    }
 }
