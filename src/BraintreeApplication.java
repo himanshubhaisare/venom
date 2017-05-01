06c967a314e6a4cda33aacb7a83f86449745a249
Himanshu Bhaisare <himanshu.bhaisare@gmail.com>
2017-04-30 18:55:01 -0700

    create jar files and test them

--- src/BraintreeApplication.java
+++ src/BraintreeApplication.java
@@ -14,6 +14,7 @@ public class BraintreeApplication {
      * @param args
      */
     public static void main(String[] args) {
+        String result = "";
         UserService userService = new UserService();
         CardService cardService = new CardService();
         Braintree braintree = new Braintree(userService, cardService);
@@ -25,9 +26,9 @@ public class BraintreeApplication {
                 Scanner scanner = new Scanner(new File(args[0]));
                 while (scanner.hasNext()) {
                     String input = scanner.nextLine();
-                    String result = braintree.handle(input);
-                    bw.write(result);
+                    result = braintree.handle(input);
                 }
+                bw.write(result);
                 bw.close();
             } catch (IOException e) {
                 e.printStackTrace();
@@ -36,7 +37,7 @@ public class BraintreeApplication {
         } else {
             System.out.println("mini braintree started in interactive mode. Enter help to see manual.");
             String input = "";
-            while (!input.equals("close")) {
+            while (!input.equals("Close")) {
                 Scanner scanner = new Scanner(System.in);
                 input = scanner.nextLine();
                 if (input.equals("help")) {
@@ -44,9 +45,10 @@ public class BraintreeApplication {
                             "Add <user> <card number> <$limit> : Adds a credit card on user with given credit limit e.g. Add Himanshu 5555555555554444 $1000\n" +
                             "Charge <user> <$amount> : Charge user with given amount, increases balance on card e.g. Charge Tom $500 \n" +
                             "Credit <user> <$amount> : Credit decreases balance on user's card by given amount e.g. Credit Lisa $100 \n" +
+                            "Close : Closes application \n" +
                             "help : brings up manual \n");
                 } else {
-                    String result = braintree.handle(input);
+                    result = braintree.handle(input);
                     System.out.print(result);
                 }
             }
