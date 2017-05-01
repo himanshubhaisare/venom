61d804bf5aac252b355f9498dbea2697cf14c1f7
Himanshu Bhaisare <himanshu.bhaisare@gmail.com>
2017-05-01 14:58:43 -0700

    Tests for cardService::charge

--- src/tests/service/CardServiceTest.java
+++ src/tests/service/CardServiceTest.java
@@ -111,4 +111,58 @@ public class CardServiceTest {
         String result = this.cardService.create(args);
         assertEquals("testCreateCardWithoutUser: ", Error.CARD_NUMBER_INVALID, result);
     }
+
+    @Test
+    public void testChargeWithoutDollarAmount() {
+        String input = "Himanshu";
+        String[] args = input.split(" ");
+        String result = this.cardService.charge(args);
+        assertEquals("testChargeWithoutDollarAmount: ", Error.INVALID_ARGS, result);
+    }
+
+    @Test
+    public void testChargeWithInvalidAmount() {
+        String input = "Himanshu 5454545454545454 $1000";
+        String[] args = input.split(" ");
+        this.cardService.create(args);
+
+        input = "Himanshu 500";
+        args = input.split(" ");
+        String result = this.cardService.charge(args);
+        assertEquals("testChargeWithInvalidAmount: ", Error.CHARGE_AMOUNT_INVALID, result);
+    }
+
+    @Test
+    public void testChargeOnUserThatDoesNotExist() {
+        Database.clearAll();
+        String input = "Himanshu $500";
+        String[] args = input.split(" ");
+        String result = this.cardService.charge(args);
+        assertEquals("testChargeOnUserThatDoesNotExist: ", Error.USER_NOT_FOUND, result);
+    }
+
+
+    @Test
+    public void testChargeOverAllowedCreditLimit() {
+        String input = "Himanshu 5454545454545454 $1000";
+        String[] args = input.split(" ");
+        this.cardService.create(args);
+
+        input = "Himanshu $1111";
+        args = input.split(" ");
+        String result = this.cardService.charge(args);
+        assertEquals("testChargeOverAllowedCreditLimit: ", Error.CHARGE_DECLINED, result);
+    }
+
+    @Test
+    public void testChargeWithDollarAmount() {
+        String input = "Himanshu 5454545454545454 $1000";
+        String[] args = input.split(" ");
+        this.cardService.create(args);
+
+        input = "Himanshu $500";
+        args = input.split(" ");
+        String result = this.cardService.charge(args);
+        assertEquals("testChargeWithDollarAmount: ", "", result);
+    }
 }
